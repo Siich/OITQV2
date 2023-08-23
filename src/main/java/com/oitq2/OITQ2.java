19d69d3672d7134488ef4c3746030af772ad2138
rain474 <lordraindance5@gmail.com>
2023-08-22 22:21:00 -0400

    simple JavaPlugin

---
+++ src/main/java/com/oitq2/OITQ2.java
@@ -0,0 +1,31 @@
+package com.oitq2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OITQ2 extends JavaPlugin {
+    public static OITQ2 INSTANCE;
+
+    public OITQ2() {
+
+    }
+
+    @Override
+    public void onLoad() {
+
+    }
+
+
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+        
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public static OITQ2 getInstance() {
+        return INSTANCE;
+    }
+}
