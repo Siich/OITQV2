1c422d92078c9d1356a05c2d11019fd72916b69f
rain474 <lordraindance5@gmail.com>
2023-09-14 23:25:42 -0400

    Added game class

--- src/main/java/com/oitq2/OITQ2.java
+++ src/main/java/com/oitq2/OITQ2.java
@@ -1,9 +1,14 @@
 package com.oitq2;
 
+import com.oitq2.game.Game;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OITQ2 extends JavaPlugin {
     public static OITQ2 INSTANCE;
+    /**current game, we keep track this, and if we want to reset game, we just redefine this variable */
+    private static Game currentGame;
 
     public OITQ2() {
 
@@ -18,6 +23,7 @@ public class OITQ2 extends JavaPlugin {
     @Override
     public void onEnable() {
         INSTANCE = this;
+        currentGame = new Game(2);
         
     }
 
@@ -28,4 +34,5 @@ public class OITQ2 extends JavaPlugin {
     public static OITQ2 getInstance() {
         return INSTANCE;
     }
+
 }
