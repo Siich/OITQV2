b301c2cee57783f922db28133b80679fedfdec60
rain474 <lordraindance5@gmail.com>
2023-09-22 00:35:30 -0400

    Added dependencies

--- src/main/java/com/oitq2/OITQ2.java
+++ src/main/java/com/oitq2/OITQ2.java
@@ -1,12 +1,19 @@
 package com.oitq2;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import com.oitq2.game.Game;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import io.github.rysefoxx.inventory.plugin.pagination.InventoryManager;
+import net.megavex.scoreboardlibrary.api.ScoreboardLibrary;
+import net.megavex.scoreboardlibrary.api.exception.NoPacketAdapterAvailableException;
+import net.megavex.scoreboardlibrary.api.noop.NoopScoreboardLibrary;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OITQ2 extends JavaPlugin {
     public static OITQ2 INSTANCE;
+    private final InventoryManager inventoryManager = new InventoryManager(this);
+    private ProtocolManager protocolLib;
+    private ScoreboardLibrary scoreboardLibrary;
     /**current game, we keep track this, and if we want to reset game, we just redefine this variable */
     private static Game currentGame;
 
@@ -17,23 +24,48 @@ public class OITQ2 extends JavaPlugin {
 
     @Override
     public void onLoad() {
-
+        //packets
+        protocolLib = ProtocolLibrary.getProtocolManager();
     }
 
 
     @Override
     public void onEnable() {
         INSTANCE = this;
+        // inventory
+        inventoryManager.invoke();
+        // scoreboard
+        try {
+            scoreboardLibrary = ScoreboardLibrary.loadScoreboardLibrary(this);
+        } catch (NoPacketAdapterAvailableException e) {
+            scoreboardLibrary = new NoopScoreboardLibrary();
+        }
+
+        //game
         currentGame = new Game(2);
-        
+
     }
 
     @Override
     public void onDisable() {
+        scoreboardLibrary.close();
     }
 
+    /** PUT GETTERS BELOW HERE */
+    
     public static OITQ2 getInstance() {
         return INSTANCE;
     }
 
+    public InventoryManager getInventoryManager() {
+        return inventoryManager;
+    }
+
+    public ProtocolManager getProtocolLib() {
+        return protocolLib;
+    }
+
+    public ScoreboardLibrary getScoreboardLibrary() {
+        return scoreboardLibrary;
+    }
 }
